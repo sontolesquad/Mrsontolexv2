873da383163b9af0572550b0715c142f5842c16f
sontolesquad <39442325+sontolesquad@users.noreply.github.com>
2018-06-03 02:29:04 +0700

    Add files via upload

---
+++ module/spam.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title> Email
+Spammer
+    </title>
+  <style>    body{     background-image: url(https://2.bp.blogspot.com/-WFCyuyiYK90/Wuhjt6KXqkI/AAAAAAAAAZw/SnBSvXODxbg1E0Y4GkdHyF6Q_76TK-efwCLcBGAs/s320/ganteng.jpg?ver=6);     background-repeat: no-repeat;      background-attachment: fixed;      background-position: top;     background-color:#000000;     position: relative;     background-size:100% 100vh;          }    .logo {     width: 300px;     height: 300px;     margin: 0 auto;     margin-top: 50px;     -webkit-filter: drop-shadow(0px 0px 7px #0080FF);     filter: drop-shadow(0px 0px 7px #0080FF);    }    .logo:hover{     width: 300px;     height: 300px;     -webkit-filter: drop-shadow(0px 0px 10px #0080FF);     opacity:0.4;     filter:alpha(opacity=40);     transition: opacity .2s ease-out;     -moz-transition: opacity .2s ease-out;     -webkit-transition: opacity .2s ease-out;     -o-transition: opacity .2s ease-out;    }    .defacedby{     font-family: Megrim;     text-align: center;     color: black;     font-weight: bold;     font-size: 50px;   text-shadow: #0080FF 1px 2px 1px;        }    .glow {     font-family: Quicksand;     text-align: center;     color: grey;     font-style: bold;     font-size: 15px;    margin-top: 16px;    text-shadow: black 1px 2px 1px;        }    .greetings{     font-family: Quicksand;     text-align: center;     color: #ffffff;     font-size: 15px;     margin-top: 50px; text-shadow: black 1px 2px 1px;    }    </style> 
+ </head> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+    </script>
+  </head>
+  <body>
+    <div class="container">
+      <br>
+      <font size=6><font color="red"><b><center>Spammer Email Create By Mrsontolex</b></center>
+      </font>
+      <style>
+        body {
+          background-color:white;
+          color:black
+        }
+        .content {
+          max-width: 700px;
+          margin: auto;
+        }
+      </style>
+      </head>
+    <div class="container content">
+      <div class="card" style="background-color: inherit; color: white; border-style: none;">
+        <center>
+          <font color="red">
+          </font>
+        </center>
+      </div>
+      </font>
+    </center>
+  </div>
+</center>
+<br>
+<form method="POST">
+  <div class="form-group">
+    <div class="input-group">
+      <span class="input-group-addon">Target :
+      </span>
+      <input type="text" placeholder="" class="form-control" name="email" autofocus="" required="">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="input-group">
+      <span class="input-group-addon">Amount :
+      </span>
+      <input type="number" class="form-control" placeholder="" name="kali" required="">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="pesan">Message :
+    </label>
+    <textarea class="form-control" rows="7" name="pesan">
+    </textarea>
+  </div>
+  <button type="submit" style="width: 100%;" class="btn btn-danger" name="ok">Submit
+  </button>
+</form>
+</table>
+<br>
+<center>
+    <center>
+    </b>
+  </center>
+  </div>
+</head>
+<body>
+  </html>
