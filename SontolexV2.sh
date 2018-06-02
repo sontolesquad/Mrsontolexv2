2b86253f8063ac7881e53855ac668c44dd6bc680
sontolesquad <39442325+sontolesquad@users.noreply.github.com>
2018-06-03 02:18:56 +0700

    Update and rename BAJINGAN.sh to SontolexV2.sh

--- SontolexV2.sh
+++ SontolexV2.sh
@@ -20,14 +20,14 @@ echo $red
 cat asw.txt
 echo ""
 echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
-echo "\033[32;1mAuthor : Mr.B4J1N64N"
-echo "\033[37;1mFeat: Mr.IM81"
-echo "\033[35;1mversion Tools: 6"
-echo "\033[35;1mTeam : DARKNESS CYBER TEAM"
-echo "\033[33;1mKontak Me :  083108271123"
+echo "\033[32;1mAuthor : Mrsontolex"
+echo "\033[37;1mFeat: Fucking Shit"
+echo "\033[35;1mversion Tools: 2"
+echo "\033[35;1mTeam : PERSIT2K!8"
+echo "\033[33;1mKontak Me :  089628368691"
 echo "\033[31;1mPlease For Subcribe Youtube:"
-echo "\033[92mDarkness Cyber Team" 
-echo "\033[36;1mIndonesia Security Lite"
+echo "\033[92mSontole Squad" 
+echo "\033[36;1mSontole Squad"
 echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
 echo ""
 echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
@@ -436,12 +436,12 @@ echo "[===============================================]>"
 echo $yellow "0.> keluar"
 echo "[+]===============================================[+]"
 echo "\033[32;1m"
-read -p "root@T00Ls-B4J1N64Nv6~#" bro
+read -p "root@T00Ls-Mrsontolexv2~#" bro
 
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
-figlet "Mr.B4J1N64N" | lolcat
+figlet "Mrsontolex" | lolcat
 php tp.php
 fi
 
@@ -449,111 +449,111 @@ if
 [ $bro = 2 ] || [ $bro = 2 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 php t.php
 fi
 
 if [ $bro = 3 ] || [ $bro = 3 ]
 then
-figlet "Mr.B4J1N64N"
+figlet "Mrsontolex"
 php mataharimall.php
 fi
 
 if [ $bro = 4 ] || [ $bro = 4 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 php hooq.php
 fi
 
 if [ $bro = 5 ] || [ $bro = 5 ]
 then
 clear
-toilet -f slant --gay "Mr.B4J1N64N"
+toilet -f slant --gay "Mrsontolex"
 php kfc.php
 fi
 
 if [ $bro = 6 ] || [ $bro = 6 ]
 then
 clear
-toilet -f mono12 -F gay "Mr.B4J1N64N"
+toilet -f mono12 -F gay "MrSontolex"
 php phd.php
 fi
 
 if [ $bro = 7 ] || [ $bro = 7 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "MrSontolex"
 php whiskas.php
 fi
 
 if [ $bro = 8 ] || [ $bro = 8 ]
 then
 clear
-figlet "Mr.B4J1N64N"
+figlet "Mrsontolex"
 php zipay.php
 fi
 
 if [ $bro = 9 ] || [ $bro = 9 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 php key.php
 fi
 
 if [ $bro = 10 ] || [ $bro = 10 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 python2 SpamMail.py
 fi
 
 if [ $bro = 11 ] || [ $bro = 11 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 python2 whs.py
 fi
 
 if [ $bro = 12 ] || [ $bro = 12 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 python2 upf.py
 fi
 
 if [ $bro = 13 ] || [ $bro = 13 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 python2 create.py
 fi
 
 if [ $bro = 14 ] || [ $bro = 14 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 python2 dog.py
 fi
 
 if [ $bro = 15 ] || [ $bro = 15 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 python2 gam.py
 fi
 
 if [ $bro = 16 ] || [ $bro = 16 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 python2 anvima.py
 fi
 
 if [ $bro = 17 ] || [ $bro = 17 ]
 then
 clear
-toilet "Mr.B4J1N64N"
+toilet "Mrsontolex"
 python2 htmd.py
 fi
 
@@ -3315,9 +3315,16 @@ cd $HOME/Google-Dork
 python2 Google.py
 fi
 
+if [ $bro = 201 ] || [ $bro = 201 ]
+then
+clear
+toilet "Mrsontolex"
+python2 ContactMe.py
+fi
+
 if [ $bro = 0 ] || [ $bro = 00 ]
 then
-echo "\033[32;1mDARKNESS CYBER TEAM"
+echo "\033[32;1mPERSIT2K18"
 sleep 1
 echo "\033[33;1mWe Security"
 sleep 1
